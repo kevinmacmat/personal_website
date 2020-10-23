55c11c01009ecb8bd784aa89602a82ce3b540f1b
Kevin Macias-Matsuura <kevinmaciasmatsuura@gmail.com>
2020-10-22 22:06:06 -0400

    Added tableau dashboards with project descriptions. Changed about me section to have profile pic that spans whwidth of the screen.

--- tableau.js
+++ tableau.js
@@ -1,6 +1,7 @@
-function initViz() {
-	var containerDiv = document.getElementById("tableau-viz"),
-	url = "http://YOUR-SERVER/views/YOUR-VISUALIZATION";
 
-	var viz = new tableau.Viz(containerDiv, url);
-}
+// function initViz() {
+//	var containerDiv = document.getElementById("tableau-viz"),
+//	url = "https://public.tableau.com/views/SummarySeattlePDTerryStops/SUMMARY?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
+//
+//	var viz = new tableau.Viz(containerDiv, url);
+//}
